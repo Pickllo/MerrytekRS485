17a094eefbdeb98ab3bd52c32ea1f515e85890b6
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-07-29 12:38:28 +0800

    Create __init__.py

---
+++ components/merrytek_radar/__init__.py
@@ -0,0 +1,35 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID, CONF_UART_ID, CONF_DEVICE_ID
+from esphome.core import coroutine_with_priority
+
+# Declare the component's namespace
+CODEOWNERS = ["@Pickllo"]
+merrytek_radar_ns = cg.esphome_ns.namespace("merrytek_radar")
+MerrytekRadar = merrytek_radar_ns.class_(
+    "MerrytekRadar", cg.PollingComponent, cg.UARTComponent
+)
+
+# Define the configuration schema
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(MerrytekRadar),
+            cv.Required(CONF_DEVICE_ID): cv.hex_uint16_t,
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+    .extend(cv.UART_DEVICE_SCHEMA)
+)
+
+# Define the coroutine to generate the C++ code
+@coroutine_with_priority(40.0)
+async def to_code(config):
+    uart_device = await cg.get_variable(config[CONF_UART_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await cg.register_uart_device(var, config)
+    cg.add(var.set_device_id(config[CONF_DEVICE_ID]))
+
+    # Ensure this component is set up after the UART bus
+    cg.add_dependency(uart_device, var)
