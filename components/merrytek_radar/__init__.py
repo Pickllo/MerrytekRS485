f98879500e2cd3503697b8abcce558558b5b3b5a
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-07-30 15:15:07 +0800

    Update __init__.py

--- components/merrytek_radar/__init__.py
+++ components/merrytek_radar/__init__.py
@@ -2,7 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, switch, number, select, button
 from esphome.const import CONF_ID
-from esphome.core import coroutine_with_priority
 
 # Declare all the components our hub depends on
 DEPENDENCIES = ['uart', 'binary_sensor', 'sensor', 'number', 'switch', 'select', 'button']
@@ -11,9 +10,9 @@ DEPENDENCIES = ['uart', 'binary_sensor', 'sensor', 'number', 'switch', 'select',
 CODEOWNERS = ["@Pickllo"]
 merrytek_radar_ns = cg.esphome_ns.namespace("merrytek_radar")
 
-# Declare the HUB class
+# Declare the HUB class - REMOVED PollingComponent
 MerrytekRadar = merrytek_radar_ns.class_(
-    "MerrytekRadar", cg.PollingComponent, uart.UARTDevice
+    "MerrytekRadar", cg.Component, uart.UARTDevice
 )
 
 # Declare the custom entity classes that live in the C++ code
@@ -25,7 +24,7 @@ MerrytekButton = merrytek_radar_ns.class_("MerrytekButton", button.Button, cg.Co
 # Define our custom configuration key
 CONF_DEVICE_ID = "device_id"
 
-# Define the configuration schema for the main hub
+# Define the configuration schema for the main hub - REMOVED polling_component_schema
 CONFIG_SCHEMA = (
     cv.Schema(
         {
@@ -33,12 +32,11 @@ CONFIG_SCHEMA = (
             cv.Required(CONF_DEVICE_ID): cv.hex_uint16_t,
         }
     )
-    .extend(cv.polling_component_schema("60s"))
+    .extend(cv.COMPONENT_SCHEMA)
     .extend(uart.UART_DEVICE_SCHEMA)
 )
 
 # Define the coroutine to generate the C++ code for the main hub
-@coroutine_with_priority(40.0)
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
