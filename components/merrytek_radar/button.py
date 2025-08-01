70fc2a8c700b93418ece2847e33ebea0673c43b3
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-01 15:01:01 +0800

    Update button.py

--- components/merrytek_radar/button.py
+++ components/merrytek_radar/button.py
@@ -8,13 +8,13 @@ from . import MerrytekButton, MerrytekRadar
 CONF_FACTORY_RESET = "factory_reset"
 CONF_ENVIRONMENTAL_SELF_LEARNING = "environmental_self_learning"
 CONF_ID_EDIT_ENABLE = "id_edit_enable"
-CONF_FLIP_STATUS = "flip_status"
+CONF_STATUS_FLIP = "status_flip"
 
 BUTTONS = {
     CONF_FACTORY_RESET: (0x30, [0x01]),
     CONF_ENVIRONMENTAL_SELF_LEARNING: (0x29, [0x01]),
     CONF_ID_EDIT_ENABLE: (0x21, [0x01]),
-    CONF_FLIP_STATUS: (0x23, [0x01]),
+    CONF_STATUS_FLIP: (0x23, [0x01]),
 }
 
 # Define the configuration schema for button entities
@@ -34,7 +34,6 @@ async def to_code(config):
     sensor_type = config["type"]
     function_code, data = BUTTONS[sensor_type]
     
-    # Correct syntax for creating a std::vector<uint8_t>
     data_vector = cg.std_vector.template(cg.uint8)(data)
     
     cg.add(hub.register_configurable_button(var, function_code, data_vector))
