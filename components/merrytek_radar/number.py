d86d375743a3ba2dbd535e987d27c7f9896b659d
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-05 18:10:21 +0800

    Update number.py

--- components/merrytek_radar/number.py
+++ components/merrytek_radar/number.py
@@ -30,12 +30,20 @@ CONFIG_SCHEMA = number.NUMBER_SCHEMA.extend({
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_MERRYTEK_RADAR_ID])
     var = cg.new_Pvariable(config[CONF_ID])
+        await number.register_number(
+        var,
+        config,
+        min_value=config[CONF_MIN_VALUE],
+        max_value=config[CONF_MAX_VALUE],
+        step=config[CONF_STEP],
+    )
     await number.register_number(var, config)
 
     number_type = config[CONF_TYPE]
     function_code = NUMBERS[number_type]
     cg.add(var.set_function_code(function_code))
     cg.add(parent.register_configurable_number(config[CONF_ADDRESS], function_code, var))
+
 
 
 
