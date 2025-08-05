671afc1cc4ba02bf5736a253b8025f056c2abf3d
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-05 18:26:50 +0800

    Update number.py

--- components/merrytek_radar/number.py
+++ components/merrytek_radar/number.py
@@ -34,11 +34,11 @@ async def to_code(config):
     parent = await cg.get_variable(config[CONF_MERRYTEK_RADAR_ID])
     var = cg.new_Pvariable(config[CONF_ID])
     await number.register_number(
-    var,
-    config,
-    min_value=config[CONF_MIN_VALUE],
-    max_value=config[CONF_MAX_VALUE],
-    step=config[CONF_STEP],
+        var,
+        config,
+        min_value=config[CONF_MIN_VALUE],
+        max_value=config[CONF_MAX_VALUE],
+        step=config[CONF_STEP],
     )
     await number.register_component(var, config)
 
@@ -46,6 +46,7 @@ async def to_code(config):
     function_code = NUMBERS[number_type]
     cg.add(var.set_function_code(function_code))
     cg.add(parent.register_configurable_number(config[CONF_ADDRESS], function_code, var))
+
 
 
 
