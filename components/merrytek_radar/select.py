1de2a0df3e194da0e265383e6a0f33367ce59d6a
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-05 17:43:17 +0800

    Update select.py

--- components/merrytek_radar/select.py
+++ components/merrytek_radar/select.py
@@ -1,12 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import select
-from esphome.const import (CONF_ID, CONF_TYPE,CONF_ADDRESS,CONF_OPTIONS,CONF_PARENT_ID,)
+from esphome.const import (CONF_ID, CONF_TYPE,CONF_ADDRESS,CONF_OPTIONS,)
 from . import merrytek_radar_ns, MerrytekRadar, MerrytekSelect
 
 # Define supported select entities and their function codes
 CONF_NEAR_ZONE_SHIELDING = "near_zone_shielding"
 CONF_SENSITIVITY= "sensitivity" 
+CONF_MERRYTEK_RADAR_ID = "merrytek_radar_id"
 
 SELECTS = {
     CONF_SENSITIVITY: (0x06, ["Low", "High", "Medium"]),
@@ -14,7 +15,7 @@ SELECTS = {
 }
 
 PLATFORM_SCHEMA = select.SELECT_SCHEMA.extend({
-    cv.GenerateID(CONF_PARENT_ID): cv.use_id(MerrytekRadar),
+    cv.GenerateID(CONF_MERRYTEK_RADAR_ID): cv.use_id(MerrytekRadar),
     cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
     cv.Required(CONF_TYPE): cv.one_of(*SELECTS, lower=True),
     cv.GenerateID(CONF_ID): cv.declare_id(MerrytekSelect),
@@ -22,7 +23,7 @@ PLATFORM_SCHEMA = select.SELECT_SCHEMA.extend({
 
 # Generate C++ code
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_PARENT_ID])
+    parent = await cg.get_variable(config[CONF_MERRYTEK_RADAR_ID])
     var = cg.new_Pvariable(config[CONF_ID])
     select_type = config[CONF_TYPE]
     function_code, options = SELECTS[select_type]
@@ -30,5 +31,6 @@ async def to_code(config):
     await select.register_select(var, config)
     cg.add(var.set_function_code(function_code))
     cg.add(parent.register_configurable_select(config[CONF_ADDRESS], function_code, var))
+
 
 
