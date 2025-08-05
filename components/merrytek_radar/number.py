e092cd928a122161cbca74dc7a32ee6368d6b4be
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-05 17:42:39 +0800

    Update number.py

--- components/merrytek_radar/number.py
+++ components/merrytek_radar/number.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import number
-from esphome.const import (CONF_ID, CONF_TYPE,CONF_ADDRESS,CONF_PARENT_ID,)
+from esphome.const import (CONF_ID, CONF_TYPE,CONF_ADDRESS,)
 from . import merrytek_radar_ns, MerrytekRadar, MerrytekNumber
 
 # Define supported number entities and their function codes
@@ -10,6 +10,7 @@ CONF_HOLD_TIME = "hold_time"
 CONF_BLOCKING_TIME = "blocking_time"
 CONF_LUX_DIFFERENCE_THRESHOLD = "lux_difference_threshold"
 CONF_DAYLIGHT_THRESHOLD = "daylight_threshold"
+CONF_MERRYTEK_RADAR_ID = "merrytek_radar_id"
 
 NUMBERS = {
     CONF_DETECTION_AREA: 0x05,
@@ -20,14 +21,14 @@ NUMBERS = {
 }
 
 PLATFORM_SCHEMA = number.NUMBER_SCHEMA.extend({
-    cv.GenerateID(CONF_PARENT_ID): cv.use_id(MerrytekRadar),
+    cv.GenerateID(CONF_MERRYTEK_RADAR_ID): cv.use_id(MerrytekRadar),
     cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
     cv.Required(CONF_TYPE): cv.one_of(*NUMBERS, lower=True),
     cv.GenerateID(CONF_ID): cv.declare_id(MerrytekNumber),
 }).extend(cv.PLATFORM_SCHEMA)
 # Generate C++ code
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_PARENT_ID])
+    parent = await cg.get_variable(config[CONF_MERRYTEK_RADAR_ID])
     var = cg.new_Pvariable(config[CONF_ID])
     await number.register_number(var, config)
 
@@ -35,5 +36,6 @@ async def to_code(config):
     function_code = NUMBERS[number_type]
     cg.add(var.set_function_code(function_code))
     cg.add(parent.register_configurable_number(config[CONF_ADDRESS], function_code, var))
+
 
 
