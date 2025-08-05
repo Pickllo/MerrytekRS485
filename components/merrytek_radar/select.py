e9ad5436de3ab5cdd1725fc977eea6f130690346
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-05 15:04:32 +0800

    Update select.py

--- components/merrytek_radar/select.py
+++ components/merrytek_radar/select.py
@@ -1,35 +1,32 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import select
-from esphome.const import CONF_ID, CONF_SENSITIVITY
-from . import MerrytekSelect, MerrytekRadar
+from esphome.const import (CONF_ID, CONF_TYPE,CONF_ADDRESS,CONF_OPTIONS)
+from . import merrytek_radar_ns, MerrytekRadar, MerrytekSelect
 
 # Define supported select entities and their function codes
 CONF_NEAR_ZONE_SHIELDING = "near_zone_shielding"
-CONF_DETECTION_AREA = "detection_area"
+CONF_SENSITIVITY= "sensitivity" 
 
 SELECTS = {
     CONF_SENSITIVITY: (0x06, ["Low", "High", "Medium"]),
     CONF_NEAR_ZONE_SHIELDING: (0x33, ["0m", "0.6m", "1.2m", "1.6m"]),
-    CONF_DETECTION_AREA: (0x05, ["0m", "0.5m", "1m", "1.5m", "2m", "2.5m", "3m", "3.5m", "4m"]),
 }
 
-# Define the configuration schema for select entities
-CONFIG_SCHEMA = select.SELECT_SCHEMA.extend({
+PLATFORM_SCHEMA = select.SELECT_SCHEMA.extend({
+    cv.GenerateID(cg.PARENT_ID): cv.use_id(MerrytekRadar),
+    cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
+    cv.Required(CONF_TYPE): cv.one_of(*SELECTS, lower=True),
     cv.GenerateID(CONF_ID): cv.declare_id(MerrytekSelect),
-    cv.Required("merrytek_radar_id"): cv.use_id(MerrytekRadar),
-    cv.Required("type"): cv.one_of(*SELECTS, lower=True),
-}).extend(cv.COMPONENT_SCHEMA)
+})
 
 # Generate C++ code
 async def to_code(config):
-    hub = await cg.get_variable(config["merrytek_radar_id"])
+    parent = await cg.get_variable(config[cg.PARENT_ID])
     var = cg.new_Pvariable(config[CONF_ID])
-    
-    sensor_type = config["type"]
-    function_code, options = SELECTS[sensor_type]
-    
-    await select.register_select(var, config, options=options)
-    await cg.register_component(var, config)
-    
-    cg.add(hub.register_configurable_select(var, function_code))
+    select_type = config[CONF_TYPE]
+    function_code, options = SELECTS[select_type]
+    config[CONF_OPTIONS] = options
+    await select.register_select(var, config)
+    cg.add(var.set_function_code(function_code))
+    cg.add(parent.register_configurable_select(config[CONF_ADDRESS], function_code, var))
