ec8293060d54ff3150ada3f38e65c19599d4d2ac
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-05 15:02:38 +0800

    Update binary_sensor.py

--- components/merrytek_radar/binary_sensor.py
+++ components/merrytek_radar/binary_sensor.py
@@ -3,6 +3,8 @@ import esphome.config_validation as cv
 from esphome.components import binary_sensor
 from esphome.const import (
     CONF_ID,
+    CONF_TYPE,
+    CONF_ADDRESS,
     DEVICE_CLASS_OCCUPANCY,
 )
 from . import MerrytekRadar, merrytek_radar_ns
@@ -13,22 +15,16 @@ BINARY_SENSORS = {
     CONF_PRESENCE: 0x03,
 }
 
-# Define the configuration schema for binary sensors
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_ID): cv.declare_id(binary_sensor.BinarySensor),
-        cv.Required("merrytek_radar_id"): cv.use_id(MerrytekRadar),
-        cv.Required("type"): cv.one_of(*BINARY_SENSORS, lower=True),
-    }
-).extend(binary_sensor.BINARY_SENSOR_SCHEMA)
-
+PLATFORM_SCHEMA = binary_sensor.BINARY_SENSOR_PLATFORM_SCHEMA.extend({
+    cv.GenerateID(cg.PARENT_ID): cv.use_id(MerrytekRadar),
+    cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
+    cv.Required(CONF_TYPE): cv.one_of(*BINARY_SENSORS, lower=True),
+})
 # Generate C++ code
 async def to_code(config):
-    hub = await cg.get_variable(config["merrytek_radar_id"])
-    var = cg.new_Pvariable(config[CONF_ID])
-    await binary_sensor.register_binary_sensor(var, config)
-
-    sensor_type = config["type"]
+    parent = await cg.get_variable(config[cg.PARENT_ID])
+    var = await binary_sensor.new_binary_sensor(config)
+    sensor_type = config[CONF_TYPE]
     if sensor_type == CONF_PRESENCE:
-        cg.add(hub.set_presence_binary_sensor(var))
         cg.add(var.set_device_class(DEVICE_CLASS_OCCUPANCY))
+    cg.add(parent.register_binary_sensor(config[CONF_ADDRESS], sensor_type, var))
