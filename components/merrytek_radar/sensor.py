2ac08e98d83c22e7206c1e83401fcb5399c02d3e
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-05 17:32:00 +0800

    Update sensor.py

--- components/merrytek_radar/sensor.py
+++ components/merrytek_radar/sensor.py
@@ -5,6 +5,7 @@ from esphome.const import (
     CONF_ID,
     CONF_TYPE,
     CONF_ADDRESS,
+    CONF_PARENT_ID,
     CONF_UNIT_OF_MEASUREMENT,
     CONF_ICON,
     CONF_STATE_CLASS,
@@ -26,13 +27,13 @@ SENSORS = {
 }
 
 PLATFORM_SCHEMA = sensor.SENSOR_SCHEMA.extend({
-    cv.GenerateID(cg.PARENT_ID): cv.use_id(MerrytekRadar),
+    cv.GenerateID(CONF_PARENT_ID): cv.use_id(MerrytekRadar),
     cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
     cv.Required(CONF_TYPE): cv.one_of(*SENSORS, lower=True),
 }).extend(cv.PLATFORM_SCHEMA)
 
 async def to_code(config):
-    parent = await cg.get_variable(config[cg.PARENT_ID])
+    parent = await cg.get_variable(config[CONF_PARENT_ID])
     var = await sensor.new_sensor(config)
     sensor_type = config[CONF_TYPE]
     function_code = SENSORS[sensor_type]
@@ -45,4 +46,5 @@ async def to_code(config):
         if CONF_ICON not in config:
             cg.add(var.set_icon(ICON_CHIP))
     cg.add(parent.register_configurable_sensor(config[CONF_ADDRESS], function_code, var))
+
 
