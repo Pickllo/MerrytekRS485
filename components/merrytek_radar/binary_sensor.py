8efb8ffc678844e3af3d4f60689e12f11f2fef1d
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-05 17:48:04 +0800

    Update binary_sensor.py

--- components/merrytek_radar/binary_sensor.py
+++ components/merrytek_radar/binary_sensor.py
@@ -19,7 +19,7 @@ PLATFORM_SCHEMA = binary_sensor.BINARY_SENSOR_SCHEMA.extend({
     cv.GenerateID(CONF_MERRYTEK_RADAR_ID): cv.use_id(MerrytekRadar),
     cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
     cv.Required(CONF_TYPE): cv.one_of(*BINARY_SENSORS, lower=True),
-}).extend(cv.PLATFORM_SCHEMA)
+})
 # Generate C++ code
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_MERRYTEK_RADAR_ID])
@@ -28,6 +28,7 @@ async def to_code(config):
     if sensor_type == CONF_PRESENCE:
         cg.add(var.set_device_class(DEVICE_CLASS_OCCUPANCY))
     cg.add(parent.register_binary_sensor(config[CONF_ADDRESS], sensor_type, var))
+
 
 
 
