f18158ce8ee9b89e7c5d3a3df63d09efd0aade1d
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-05 15:03:59 +0800

    Update number.py

--- components/merrytek_radar/number.py
+++ components/merrytek_radar/number.py
@@ -1,13 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import number
-from esphome.const import (
-    CONF_ID,
-    CONF_MAX_VALUE,
-    CONF_MIN_VALUE,
-    CONF_STEP,
-)
-from . import MerrytekNumber, MerrytekRadar
+from esphome.const import (CONF_ID, CONF_TYPE,CONF_ADDRESS,)
+from . import merrytek_radar_ns, MerrytekRadar, MerrytekNumber
 
 # Define supported number entities and their function codes
 CONF_DETECTION_AREA = "detection_area"
@@ -24,31 +19,19 @@ NUMBERS = {
     CONF_DAYLIGHT_THRESHOLD: 0x25,
 }
 
-# Define the configuration schema for number entities
-CONFIG_SCHEMA = number.NUMBER_SCHEMA.extend({
+PLATFORM_SCHEMA = number.NUMBER_PLATFORM_SCHEMA.extend({
+    cv.GenerateID(cg.PARENT_ID): cv.use_id(MerrytekRadar),
+    cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
+    cv.Required(CONF_TYPE): cv.one_of(*NUMBERS, lower=True),
     cv.GenerateID(CONF_ID): cv.declare_id(MerrytekNumber),
-    cv.Required("merrytek_radar_id"): cv.use_id(MerrytekRadar),
-    cv.Required("type"): cv.one_of(*NUMBERS, lower=True),
-    cv.Optional(CONF_MIN_VALUE, default=0.0): cv.float_,
-    cv.Optional(CONF_MAX_VALUE, default=255.0): cv.float_,
-    cv.Optional(CONF_STEP, default=1.0): cv.positive_float,
-}).extend(cv.COMPONENT_SCHEMA)
-
-
+})
 # Generate C++ code
 async def to_code(config):
-    hub = await cg.get_variable(config["merrytek_radar_id"])
+    parent = await cg.get_variable(config[cg.PARENT_ID])
     var = cg.new_Pvariable(config[CONF_ID])
-    
-    await number.register_number(
-        var,
-        config,
-        min_value=config[CONF_MIN_VALUE],
-        max_value=config[CONF_MAX_VALUE],
-        step=config[CONF_STEP],
-    )
-    await cg.register_component(var, config)
+    await number.register_number(var, config)
 
-    sensor_type = config["type"]
-    function_code = NUMBERS[sensor_type]
-    cg.add(hub.register_configurable_number(var, function_code))
+    number_type = config[CONF_TYPE]
+    function_code = NUMBERS[number_type]
+    cg.add(var.set_function_code(function_code))
+    cg.add(parent.register_configurable_number(config[CONF_ADDRESS], function_code, var))
