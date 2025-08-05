395db1044b75c4a389bd94a15efbc8627d1308c4
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-05 17:25:33 +0800

    Update switch.py

--- components/merrytek_radar/switch.py
+++ components/merrytek_radar/switch.py
@@ -26,12 +26,12 @@ SWITCHS = {
 }
 
 # Define the configuration schema for switch entities, updated for the new architecture
-PLATFORM_SCHEMA = switch.SWITCH_PLATFORM_SCHEMA.extend({
+PLATFORM_SCHEMA = switch.SWITCH_SCHEMA.extend({
     cv.GenerateID(cg.PARENT_ID): cv.use_id(MerrytekRadar),
     cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
     cv.Required(CONF_TYPE): cv.one_of(*SWITCHS, lower=True),
     cv.GenerateID(CONF_ID): cv.declare_id(MerrytekSwitch),
-})
+}).extend(cv.PLATFORM_SCHEMA)
 
 # Generate C++ code
 async def to_code(config):
@@ -42,6 +42,7 @@ async def to_code(config):
     function_code = SWITCHS[switch_type]
     cg.add(var.set_function_code(function_code))
     cg.add(parent.register_configurable_switch(config[CONF_ADDRESS], function_code, var))
+
 
 
 
