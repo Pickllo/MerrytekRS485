c0a239880351ecc7a990c005113eed0c75aea45e
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-06 12:00:03 +0800

    Update __init__.py

--- components/merrytek_radar/__init__.py
+++ components/merrytek_radar/__init__.py
@@ -1,16 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, button, number, select, switch
+from esphome.components import uart
 from esphome.const import CONF_ID, CONF_ADDRESS, CONF_MODEL, CONF_NAME
 
-# Declare the component's namespace
 CODEOWNERS = ["@Pickllo"]
+MULTI_CONF = True
+
 merrytek_radar_ns = cg.esphome_ns.namespace("merrytek_radar")
-MerrytekRadar = merrytek_radar_ns.class_( "MerrytekRadar", cg.PollingComponent, uart.UARTDevice)
-MerrytekSwitch = merrytek_radar_ns.class_("MerrytekSwitch", switch.Switch, cg.Component)
-MerrytekNumber = merrytek_radar_ns.class_("MerrytekNumber", number.Number, cg.Component)
-MerrytekSelect = merrytek_radar_ns.class_("MerrytekSelect", select.Select, cg.Component)
-MerrytekButton = merrytek_radar_ns.class_("MerrytekButton", button.Button, cg.Component)
+MerrytekRadar = merrytek_radar_ns.class_("MerrytekRadar", cg.PollingComponent, uart.UARTDevice)
+
+MerrytekSwitch = merrytek_radar_ns.class_("MerrytekSwitch", cg.Component)
+MerrytekNumber = merrytek_radar_ns.class_("MerrytekNumber", cg.Component)
+MerrytekSelect = merrytek_radar_ns.class_("MerrytekSelect", cg.Component)
+MerrytekButton = merrytek_radar_ns.class_("MerrytekButton", cg.Component)
+MerrytekSensor = merrytek_radar_ns.class_("MerrytekSensor", cg.Component)
+MerrytekTextSensor = merrytek_radar_ns.class_("MerrytekTextSensor", cg.Component)
 
 MODELS = ["msa237d", "msa236d"]
 
@@ -19,20 +23,23 @@ DEVICE_SCHEMA = cv.Schema({
     cv.Required(CONF_MODEL): cv.one_of(*MODELS, lower=True),
     cv.Required(CONF_NAME): cv.string,
 })
-# Define the configuration schema
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(MerrytekRadar),
-    
-    # The new, required way to define devices
-    cv.Required("devices"): cv.ensure_list(DEVICE_SCHEMA),
 
-}).extend(cv.polling_component_schema("60s")).extend(uart.UART_DEVICE_SCHEMA)
+CONFIG_SCHEMA = (
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(MerrytekRadar),
+        cv.Required("devices"): cv.ensure_list(DEVICE_SCHEMA),
+    })
+    .extend(cv.polling_component_schema("60s"))
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
 
-
-# Define the coroutine to generate the C++ code
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
     for device_conf in config["devices"]:
-        cg.add(var.register_device(device_conf[CONF_NAME], device_conf[CONF_ADDRESS], device_conf[CONF_MODEL], ))
+        cg.add(var.register_device(
+            device_conf[CONF_NAME], 
+            device_conf[CONF_ADDRESS], 
+            device_conf[CONF_MODEL]
+        ))
