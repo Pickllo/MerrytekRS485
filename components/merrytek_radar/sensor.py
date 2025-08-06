64704c0f0fad7b2b1820b5ea38e49371ab6b8d98
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-06 12:00:47 +0800

    Update sensor.py

--- components/merrytek_radar/sensor.py
+++ components/merrytek_radar/sensor.py
@@ -2,61 +2,41 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
-    CONF_ADDRESS,
-    CONF_ICON,
     CONF_ID,
-    CONF_STATE_CLASS,
     CONF_TYPE,
+    CONF_ADDRESS,
     CONF_UNIT_OF_MEASUREMENT,
-    ICON_CHIP,
+    CONF_DEVICE_CLASS,
+    CONF_STATE_CLASS,
+    DEVICE_CLASS_ILLUMINANCE,
     STATE_CLASS_MEASUREMENT,
     UNIT_LUX,
 )
+from . import MerrytekRadar, merrytek_radar_ns
 
-from . import merrytek_radar_ns, MerrytekRadar
+TYPES = ["light_level"]
 
-CONF_LIGHT_LEVEL = "light_level"
-CONF_DIFFERENCE_VALUE = "difference_value"
-CONF_FIRMWARE_VERSION = "firmware_version"
-CONF_MERRYTEK_RADAR_ID = "merrytek_radar_id"
-
-SENSORS = {
-    CONF_LIGHT_LEVEL: 0x09,
-    CONF_DIFFERENCE_VALUE: 0x0A,
-    CONF_FIRMWARE_VERSION: 0x17,
-}
-
-CONFIG_SCHEMA = sensor.SENSOR_SCHEMA.extend(
-    {
-        cv.GenerateID(CONF_MERRYTEK_RADAR_ID): cv.use_id(MerrytekRadar),
-        cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
-        cv.Required(CONF_TYPE): cv.one_of(*SENSORS, lower=True),
-        cv.Optional(CONF_STATE_CLASS): cv.enum(sensor.STATE_CLASSES, upper=True),
-    }
-)
+CONFIG_SCHEMA = sensor.sensor_schema(
+    unit_of_measurement=str,
+    device_class=str,
+    state_class=str,
+).extend({
+    cv.GenerateID(): cv.declare_id(sensor.Sensor),
+    cv.Required("merrytek_radar_id"): cv.use_id(MerrytekRadar),
+    cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
+    cv.Required(CONF_TYPE): cv.one_of(*TYPES, lower=True),
+})
 
 async def to_code(config):
-    parent = await cg.get_variable(config[CONF_MERRYTEK_RADAR_ID])
+    parent = await cg.get_variable(config["merrytek_radar_id"])
+    var = cg.new_Pvariable(config[CONF_ID])
+    await sensor.register_sensor(var, config)
 
-    config_copy = config.copy()
-    state_class = config_copy.pop(CONF_STATE_CLASS, None)
-    
-    var = await sensor.new_sensor(config_copy)
-
-    if state_class is not None:
-        cg.add(var.set_state_class(state_class))
-    
-    sensor_type = config[CONF_TYPE]
-    function_code = SENSORS[sensor_type]
-
-    if sensor_type == CONF_LIGHT_LEVEL:
-        if CONF_UNIT_OF_MEASUREMENT not in config:
-            cg.add(var.set_unit_of_measurement(UNIT_LUX))
-        if state_class is None:
-            cg.add(var.set_state_class(STATE_CLASS_MEASUREMENT))
-
-    elif sensor_type == CONF_FIRMWARE_VERSION:
-        if CONF_ICON not in config:
-            cg.add(var.set_icon(ICON_CHIP))
-
-    cg.add(parent.register_configurable_sensor(config[CONF_ADDRESS], function_code, var))
+    if config[CONF_TYPE] == "light_level":
+        cg.add(var.set_parent(parent))
+        cg.add(var.set_address(config[CONF_ADDRESS]))
+        cg.add(var.set_value_type(0))
+        cg.add(var.set_device_class(DEVICE_CLASS_ILLUMINANCE))
+        cg.add(var.set_unit_of_measurement(UNIT_LUX))
+        cg.add(var.set_state_class(STATE_CLASS_MEASUREMENT))
+        cg.add(parent.register_sensor_listener(var))
