227e8d3e88466c31211c6d1eb3272c2cff76f759
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-08-06 18:11:33 +0800

    Update select.py

--- components/merrytek_radar/select.py
+++ components/merrytek_radar/select.py
@@ -9,12 +9,14 @@ from . import merrytek_radar_ns, MerrytekRadar, MerrytekSelect
 CONF_NEAR_ZONE_SHIELDING = "near_zone_shielding"
 CONF_SENSITIVITY= "sensitivity" 
 CONF_DETECTION_AREA= "detection_area" 
+CONF_MOTION_SENSITIVITY = "motion_sensitivity"
 CONF_MERRYTEK_RADAR_ID = "merrytek_radar_id"
 
 SELECTS = {
     CONF_SENSITIVITY: (0x06, ["Low", "High", "Medium"]),
     CONF_NEAR_ZONE_SHIELDING: (0x33, ["0m", "0.6m", "1.2m", "1.6m"]),
-    CONF_DETECTION_AREA: (0x05, None), # Options will be set dynamically below
+    CONF_DETECTION_AREA: (0x05, ["0m", "0.5m", "1m", "1.5m", "2m", "2.5m", "3m", "3.5m", "4m"]),
+    CONF_MOTION_SENSITIVITY: (0x05, ["0%", "25%", "50%", "75%", "100%"],
 }
 
 CONFIG_SCHEMA = select.SELECT_SCHEMA.extend({
@@ -26,29 +28,14 @@ CONFIG_SCHEMA = select.SELECT_SCHEMA.extend({
 
 # Generate C++ code
 async def to_code(config):
+    parent = await cg.get_variable(config[CONF_MERRYTEK_RADAR_ID])
     var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
     select_type = config[CONF_TYPE]
     function_code, options = SELECTS[select_type]
-    if options is None:
-        address = config[CONF_ADDRESS]
-        parent_id = config[CONF_MERRYTEK_RADAR_ID]
-        device_model = ""
-        for parent_config in CORE.config["merrytek_radar"]:
-            if parent_config[CONF_ID] == parent_id:
-                for device_config in parent_config["devices"]:
-                    if device_config[CONF_ADDRESS] == address:
-                        device_model = device_config[CONF_MODEL]
-                        break
-                break
-        if not device_model:
-            raise cv.Invalid(f"Could not find a device with address {address} in the merrytek_radar configuration.")
-        if device_model == "msa236d":
-            options = ["0%", "25%", "50%", "75%", "100%"]
-        else:  
-            options = ["0m", "0.5m", "1m", "1.5m", "2m", "2.5m", "3m", "3.5m", "4m"]  
     await select.register_select(var, config, options=options)
-    await cg.register_component(var, config)
-
-    parent = await cg.get_variable(config[CONF_MERRYTEK_RADAR_ID])
     cg.add(var.set_function_code(function_code))
-    cg.add(parent.register_configurable_select(config[CONF_ADDRESS], function_code, var))
+    behavior = MerrytekSelect.SEND_INDEX 
+    if select_type == CONF_MOTION_SENSITIVITY:
+        behavior = MerrytekSelect.SEND_PERCENTAGE_VALUE
+    cg.add(parent.register_configurable_select(config[CONF_ADDRESS], function_code, var, behavior))
