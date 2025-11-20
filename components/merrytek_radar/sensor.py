b3871ab4b159235a43a9b1bf4e03b0dcccb09650
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-11-20 16:20:47 +0800

    Update sensor.py

--- components/merrytek_radar/sensor.py
+++ components/merrytek_radar/sensor.py
@@ -10,10 +10,9 @@ from esphome.const import (
     UNIT_LUX,
     ICON_LIGHTBULB,
 )
+from . import merrytek_radar_ns, MerrytekRadar, MerrytekSensor
 
-from . import merrytek_radar_ns, MerrytekRadar
-
-MerrytekSensor = merrytek_radar_ns.class_("MerrytekSensor", sensor.Sensor, cg.Component)
+CONF_MERRYTEK_RADAR_ID = "merrytek_radar_id"
 
 TYPES = ["light_level"]
 
@@ -22,25 +21,28 @@ FUNCTION_CODES = {
 }
 
 CONFIG_SCHEMA = sensor.sensor_schema(
+    MerrytekSensor,
     unit_of_measurement=UNIT_LUX,
     device_class=DEVICE_CLASS_ILLUMINANCE,
     state_class=STATE_CLASS_MEASUREMENT,
     icon=ICON_LIGHTBULB,
 ).extend({
-    cv.GenerateID(): cv.declare_id(MerrytekSensor),
-    cv.Required("merrytek_radar_id"): cv.use_id(MerrytekRadar),
+    cv.GenerateID(CONF_MERRYTEK_RADAR_ID): cv.use_id(MerrytekRadar),
     cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
     cv.Required(CONF_TYPE): cv.one_of(*TYPES, lower=True),
 })
 
 async def to_code(config):
-    parent = await cg.get_variable(config["merrytek_radar_id"])
+    parent = await cg.get_variable(config[CONF_MERRYTEK_RADAR_ID])
     var = cg.new_Pvariable(config[CONF_ID])
+    
     await sensor.register_sensor(var, config)
+    await cg.register_component(var, config)
+
     cg.add(var.set_parent(parent))
     cg.add(var.set_address(config[CONF_ADDRESS]))
+    
     function_code = FUNCTION_CODES[config[CONF_TYPE]]
     cg.add(var.set_function_code(function_code))
+    
     cg.add(parent.register_configurable_sensor(config[CONF_ADDRESS], function_code, var))
-
-
