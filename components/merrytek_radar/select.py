70b6633e86f663d6be12458adeaf2c8bdb87d4f8
Abdul Hanif Bin Murid <119839945+Pickllo@users.noreply.github.com>
2025-11-20 16:18:58 +0800

    Update select.py

--- components/merrytek_radar/select.py
+++ components/merrytek_radar/select.py
@@ -1,16 +1,16 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import select
-from esphome.const import (CONF_ID, CONF_TYPE,CONF_ADDRESS,)
+from esphome.const import (CONF_ID, CONF_TYPE, CONF_ADDRESS)
 from esphome.core import CORE
 from . import merrytek_radar_ns, MerrytekRadar, MerrytekSelect, SelectBehavior
 
-# Define supported select entities and their function codes
 CONF_NEAR_ZONE_SHIELDING = "near_zone_shielding"
 CONF_SENSITIVITY = "sensitivity"
 CONF_PRESENCE_DETECTION_AREA = "presence_detection_area"
 CONF_MOTION_DETECTION_AREA = "motion_detection_area"
 CONF_MERRYTEK_RADAR_ID = "merrytek_radar_id"
+
 SELECTS = {
     CONF_SENSITIVITY: (0x06, ["Low", "High", "Medium"]),
     CONF_NEAR_ZONE_SHIELDING: (0x33, ["0m", "0.6m", "1.2m", "1.6m"]),
@@ -18,11 +18,10 @@ SELECTS = {
     CONF_MOTION_DETECTION_AREA: (0x05, ["0%", "25%", "50%", "75%", "100%"]),
 }
 
-CONFIG_SCHEMA = select.SELECT_SCHEMA.extend({
+CONFIG_SCHEMA = select.select_schema(MerrytekSelect).extend({
     cv.GenerateID(CONF_MERRYTEK_RADAR_ID): cv.use_id(MerrytekRadar),
     cv.Required(CONF_ADDRESS): cv.hex_uint16_t,
-    cv.Required("type"): cv.one_of(*SELECTS, lower=True),
-    cv.GenerateID(CONF_ID): cv.declare_id(MerrytekSelect),
+    cv.Required(CONF_TYPE): cv.one_of(*SELECTS, lower=True),
 })
 
 # Generate C++ code
@@ -30,12 +29,15 @@ async def to_code(config):
     parent = await cg.get_variable(config[CONF_MERRYTEK_RADAR_ID])
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    select_type = config["type"]
+    
+    select_type = config[CONF_TYPE]
     function_code, options = SELECTS[select_type]
+    
     await select.register_select(var, config, options=options)
     cg.add(var.set_function_code(function_code))
+    
     behavior = SelectBehavior.SEND_INDEX
     if select_type == CONF_MOTION_DETECTION_AREA:
         behavior = SelectBehavior.SEND_PERCENTAGE_VALUE
+        
     cg.add(parent.register_configurable_select(config[CONF_ADDRESS], function_code, var, behavior))
-
